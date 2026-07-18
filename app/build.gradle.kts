c08a0d851902055f079b2e03b5f266b4644e0b73
AdiBhaiAlpha <chowdhuryadityo@gmail.com>
2026-07-18 17:54:35 +0600

    ci: automate Android launcher icon generation
    Add a step to the Android workflow to generate adaptive launcher icons from the app logo and configure aaptOptions to disable image crunching.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,12 @@ android {
             )
         }
     }
+
+    @Suppress("DEPRECATION")
+    aaptOptions {
+        isCruncherEnabled = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
